5d08effde689047859eaee676f9e5cfa85bfb92d
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2026-02-13 19:16:09 +0400

    Add Samurai (user)

--- modules/core/src/main/scala/Samurai.scala
+++ modules/core/src/main/scala/Samurai.scala
@@ -5,7 +5,11 @@ import id.IdType
 private trait Tag
 private opaque type Id = IdType[Tag]
 
+// todo: move into separate type if validation will be needed
+private opaque type Password <: String = String
+
 case class Samurai(
-    id: Id
-    
+    id: Id,
+    email: Email,
+    password: String,
 )
